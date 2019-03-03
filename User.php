df172703517f206971e28e6479dd4c539fa1a790
Tenreiro Benjamin <tenreiro.benjamin@gmail.com>
2019-03-03 16:36:40 +0100

    Added user class

---
+++ User.php
@@ -0,0 +1,64 @@
+<?php
+
+abstract class User {
+	private id;
+	private type;
+	private name;
+	private password;
+	private email;
+
+	public function __construct() {
+		$this->type = strtolower(static::class);
+	}
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function getName() {
+		return $this->name;
+	}
+
+	public function getType() {
+		return $this->type;
+	}
+
+	public function getPassword() {
+		return $this->password;
+	}
+
+	public function getEmail() {
+		return $this->email;
+	}
+
+	public function setId($id) {
+		$id = (int) $id;
+		if ($id > 0) {
+			$this->id = $id;
+		}
+	}
+
+	public function setName($name) {
+		if (is_string($name)) {
+			$this->name = $name;
+		}
+	}
+
+	public function setPassword($password) {
+		if (is_string($password)) {
+			$this->password = $password;
+		}
+	}
+
+	public function setEmail($email) {
+		if (is_string($email)) {
+			$this->email = $email;
+		}
+	}
+
+}
+
+class Admin extends User {}
+
+class Member extends User {}
+
