e2d155b77371cbb04a7bcf4b26ab71c1e371976f
tommysonsylverstone <47473393+tommysonsylverstone@users.noreply.github.com>
2019-03-08 13:32:02 +0100

    Updated post.php
    Did some syntaxe changes as well as some class renaming.

--- Post.php
+++ Post.php
@@ -6,76 +6,62 @@ class Post {
 	private $chapo;
 	private $content;
 	private $date;
-	private $author;
+	private $authorId;
 	private $lastUpdated;
 
 	public function getId() {
 		return $this->id;
 	}
+	
+	public function setId(int $id) {
+		$this->id = $id;
+	}
 
 	public function getTitle() {
 		return $this->title;
+	}
+	
+	public function setTitle(string $title) {
+		$this->title = $title;
 	}
 
 	public function getChapo() {
 		return $this->chapo;
 	}
+	
+	public function setChapo(string $chapo) {
+		$this->chapo = $chapo;
+	}
 
 	public function getContent() {
 		return $this->content;
+	}
+	
+	public function setContent(string $content) {
+		$this->content = $content;
 	}
 
 	public function getDate() {
 		return $this->date;
 	}
+	
+	public function setDate($date) {
+		$this->date = $date;
+	}
 
 	public function getAuthor() {
 		return $this->author;
+	}
+	
+	public function setAuthor(int $author) {
+		$this->author = $author;
 	}
 
 	public function getLastUpdated() {
 		return $this->lastUpdated;
 	}
-
-	public function setId($id) {
-		$id = (int) $id;
-		if ($id > 0) {
-			$this->id = $id;
-		}
-	}
-
-	public function setTitle($title) {
-		if (is_string($title)) {
-			$this->title = $title;
-		}
-	}
-
-	public function setChapo($chapo) {
-		if (is_string($chapo)) {
-			$this->chapo = $chapo;
-		}
-	}
-
-	public function setContent($content) {
-		if (is_string($content)) {
-			$this->content = $content;
-		}
-	}
-
-	public function setDate($date) {
-		$this->date = $date;
-	}
-
-	public function setAuthor($author) {
-		if (is_string($this)) {
-			$this->author = $author;
-		}
-	}
-
+	
 	public function setLastUpdated($lastUpdated) {
 		$this->lastUpdated = $lastUpdated;
 	}
 }
-
-
-
