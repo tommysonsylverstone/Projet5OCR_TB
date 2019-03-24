757f55cad320759430d75d9209a4d9758c2cafbd
Tenreiro Benjamin <tenreiro.benjamin@gmail.com>
2019-03-24 18:30:06 +0100

    added a login fonction on UserManager.php, modified BaseManager in order for it to work properly when called. Also I will need to use User file User.php in order to make in work with getters and setters.

--- UserManager.php
+++ UserManager.php
@@ -1,8 +1,17 @@
 <?php
-
+require_once('User.php');
 require_once('BaseManager.php');
 
 class UserManager extends BaseManager {
+	private $db;
+
+	//Fonction qui permet de se connecter à la DB dès que l'on appelle l'objet
+	public function __construct() {
+		$this->db = new BaseManager();
+		$this->db = $this->db->dbConnect();
+	}
+
+
 	public function addUser(User $user) {
 		$q = $this->db->prepare('INSERT INTO users(name, password, email, type) VALUES(:name, :password, :email, :type)');
 
@@ -30,5 +39,25 @@ class UserManager extends BaseManager {
 		$q->bindValue(':email', $user->getEmail(), PDO::PARAM_STR);
 
 		$q->execute();
+	}
+
+	public function login($name, $password) {
+		if (!empty($name) && !empty($password)) {
+			$q = $this->db->prepare('SELECT * FROM users WHERE name=:name and password=:password');
+
+			$q->bindValue('name', $name, PDO::PARAM_STR);
+			$q->bindValue('password', $password, PDO::PARAM_STR);
+
+			$q->execute();
+
+			if ($q->rowCount() == 1) {
+				$_SESSION['username'] = $name;
+				header('location: loginSuccess.php');
+			} else {
+				echo "Nom d'utilisateur ou mot de passe incorrect.";
+			}
+		} else {
+			echo "Please enter username and password";
+		}
 	}
 }
