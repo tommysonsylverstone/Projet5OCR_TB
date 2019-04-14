c91001e7b3dba8442e825f158d18b943d034788b
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2019-04-14 20:37:17 +0200

    rearranged login.php structure and added the template.php file into it

--- views/login.php
+++ views/login.php
@@ -4,36 +4,32 @@ include_once('includes/autoloader.php');
 
 session_start();
 
-if (isset($_POST['submit-button'])) {
-	$username = $_POST['username'];
-	$password = $_POST['pwd'];
+$username = $_POST['username'] ?? '';
+$password = $_POST['pwd'] ?? '';
 
+if (isset($_POST['submit-button'])) {
 	$uManager = new UserManager();
 	$uManager->login($username, $password);
-} ?>
+} 
 
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8" />
-	<title>Page de connexion</title>
-</head>
-<body>
-	<?php
+ob_start(); 
+
+	include('includes/header.php');
 	if (empty(isset($_SESSION['username']))) {
-		include('includes/header.php');
+
 		?>
 		<form action="login.php" method="post">
-			<input type="text" name="username" placeholder="Pseudonyme" value="<?php if (isset($_POST['username'])) { echo $_POST['username'];} else {echo '';} ?>" /><br/>
+			<input type="text" name="username" placeholder="Pseudonyme" value="<?php $username ?>" /><br/>
 			<input type="password" name="pwd" placeholder="Mot de passe" /><br/>
 			<button type="submit" name="submit-button">Se connecter</button>
 		</form>
 		<?php } else {
-			include('includes/header.php');
 		echo "Bonjour " . $_SESSION['username'] . " !";
 		?> <br/>
 		<a href="mainPageView.php">Retour à l'accueil</a> <br />
 		<a href="logout.php">Se déconnecter.</a>
-		<?php } ?>
-</body>
-</html>
+		<?php } 
+
+$content = ob_get_clean();
+
+require('includes/template.php'); ?>
