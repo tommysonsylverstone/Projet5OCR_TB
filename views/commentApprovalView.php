05e88b7b37ab55ad0ec1dd891d4e66d7541317c7
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2019-04-19 14:59:30 +0200

    restricted access to the comment approval page

--- views/commentApprovalView.php
+++ views/commentApprovalView.php
@@ -6,27 +6,33 @@ session_start();
 
 ob_start(); 
 
-
 $comments = new CommentManager();
-$invalidComm = $comments->getCommentsForAdmin(); ?>
+$invalidComm = $comments->getCommentsForAdmin(); 
 
-<table border="1">
-	<thead>
-		<tr>
-			<th>Pseudonyme</th>
-			<th>id du post</th>
-			<th>Commentaire</th>
-			<th>Valider ?</th>
-		</tr>
-	</thead>
-	<?php while ($comment = $invalidComm->fetch()) { ?>
-		<tr>
-			<td><?= $comment['authorName'] ?></td>
-			<td><?= $comment['postId'] ?></td>
-			<td><?= nl2br(htmlspecialchars($comment['content'])) ?></td>
-			<td><button>Valider</button></td>
-		</tr>
-	<?php } ?>
+new UserManager();
+$user = UserManager::getUser($_SESSION['username'] ?? empty($_SESSION['username']));
+
+if ($user['type'] == 'admin' && !empty($_SESSION['username'])) { ?>
+	<table border="1">
+		<thead>
+			<tr>
+				<th>Pseudonyme</th>
+				<th>id du post</th>
+				<th>Commentaire</th>
+				<th>Valider ?</th>
+			</tr>
+		</thead>
+		<?php while ($comment = $invalidComm->fetch()) { ?>
+			<tr>
+				<td><?= $comment['authorName'] ?></td>
+				<td><?= $comment['postId'] ?></td>
+				<td><?= nl2br(htmlspecialchars($comment['content'])) ?></td>
+				<td><button>Valider</button></td>
+			</tr>
+		<?php } 
+	} else {
+		echo "Vous n'avez pas accès à cette page";
+	} ?>
 </table>
 <?php $content = ob_get_clean();
 
