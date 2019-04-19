0bd861b9c69154b0fdf74b1f4184d54f87e799d5
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2019-04-20 01:31:44 +0200

    added a link

--- views/adminView.php
+++ views/adminView.php
@@ -4,13 +4,13 @@ include_once('includes/autoloader.php');
 
 session_start();
 
-ob_start();
-?>
+ob_start(); ?>
 
 <ul>
 	<li><a href="" class="Addpost">Ajouter un billet</a></li>
 	<li><a href="index.php?action=ViewMembersList" class="Viewmembers">Voir la liste des membres</a></li>
 	<li><a href="logout.php" class="Deconnection">Vous déconnecter</a></li>
+	<li><a href="commentApprovalView.php">Voir la liste des commentaires non validés</a></li>
 </ul>
 
 <section>Bienvenue <?= $_SESSION['username'] ?>!</section>
