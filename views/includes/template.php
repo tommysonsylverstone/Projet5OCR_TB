7d6da89bb3b6df60f74d2903f8d27cccda0497de
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2019-04-20 02:17:44 +0200

    updated template

--- views/includes/template.php
+++ views/includes/template.php
@@ -2,10 +2,17 @@
 <html>
 <head>
 	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="../public/css/style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	<title><?= $title ?></title>
 </head>
 
-<body>
-	<?= $content ?>
+<body class="body">
+	<div class="container">
+		<?php include('includes/header.php');
+		echo $content;
+		include('includes/footer.php'); ?>
+	</div>
 </body>
 </html>
