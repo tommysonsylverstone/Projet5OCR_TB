63cb0d234a6e9676d0e1f00e1f71ea8e49c4f4e9
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2019-04-21 03:42:26 +0200

    added index.php file

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+require('controller/frontend.php');
+
+try {
+	if (isset($_GET['action'])) {
+		switch ($_GET['action']) {
+			case 'addPostView':
+			addPostView();
+			break;
+
+			case 'adminView':
+			adminView();
+			break;
+
+			case 'commentApprovalView':
+			commentApprovalView();
+			break;
+
+			case 'commentValidated':
+			commentValidated();
+			break;
+
+			case 'editPostView':
+			editPostView();
+			break;
+
+			case 'login':
+			login();
+			break;
+
+			case 'loginSuccess':
+			loginSuccess();
+			break;
+
+			case 'logout':
+			logout();
+			break;
+
+			case 'mainPageView':
+			mainPageView();
+			break;
+
+			case 'postView':
+			postView();
+			break;
+
+			case 'registerSuccess':
+			registerSuccess();
+			break;
+
+			case 'registerView':
+			registerView();
+			break;
+
+			default:
+
+			break;
+		}
+	}
+	else {
+		mainPageView();
+	}
+}
+catch(Exception $e) {
+	echo 'Erreur : ' . $e->getMessage();
+}
