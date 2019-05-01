166f4fc319514f7d09c22130e38ba789ca4f54e9
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2019-05-01 16:46:41 +0200

    corrected a bug where the user could validated a comment via the url

--- views/commentValidated.php
+++ views/commentValidated.php
@@ -2,9 +2,13 @@
 
 ob_start();
 
-$valid = new CommentManager();
-$valid->validateComment($_GET['id'], TRUE);
-header("location: ?action=unapprovedList");
+if ($user->getType() !== 'admin') {
+	header('location: index.php');
+} else {
+	$valid = new CommentManager();
+	$valid->validateComment($_GET['id'], TRUE);
+	header("location: ?action=unapprovedList");
+}
 
 $content = ob_get_clean();
 
