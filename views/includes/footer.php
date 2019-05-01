a3bd5918d76f634105d03d9e24a5438b35591843
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2019-05-01 17:44:00 +0200

    updated footer

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -3,10 +3,7 @@
 		<li><a href="index.php?action=contact">Me contacter</a></li>
 		<li><a href="https://www.github.com/tommysonsylverstone/">Github</a></li>
 		<li><a href="https://www.linkedin.com/in/benjamin-tenreiro-1563a2173/">LinkedIn</a></li>
-		<?php
-		new UserManager();
-		$user = UserManager::getUser($_SESSION['username'] ?? empty($_SESSION['username']));
-		if ($user['type'] == 'admin') { ?>
+		<?php if ($user->getType() == 'admin') { ?>
 			<li><a href="?action=administration">Page d'administration</a></li>
 		<?php } ?>
 	</ul>
