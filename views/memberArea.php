3991f40262b067d817b07d15b5e81af8e2fdf493
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2019-05-11 17:08:54 +0200

    added edit email form into view

--- views/memberArea.php
+++ views/memberArea.php
@@ -4,8 +4,20 @@ $image = 'public/img/ueno_park.jpg';
 
 ob_start();
 
-include('includes/header-bg.php');
+include('includes/header-bg.php'); ?>
 
-$content = ob_get_clean();
+<section class="container">
+	<h4>Modifier votre adresse mail</h4>
+	<form action="" method="post">
+		<label>Adresse mail actuelle</label><br/>
+		<input type="text" name="email" class="form-control" required/><br/>
+		<label>Nouvelle adresse mail</label><br/>
+		<input type="text" name="new-email" class="form-control" required/><br/>
+		<label>Confirmer nouvelle adresse mail</label><br/>
+		<input type="text" name="confirm-new-email" class="form-control" required/><br/>
+		<button type="submit" name="submit-new-email" class="btn btn-primary">Valider</button>
+	</form>
+</section>
+<?php $content = ob_get_clean();
 
 require('includes/template.php');
