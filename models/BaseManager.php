b999a5582970fd28528cc34e10b15761007efd66
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2019-05-15 05:01:11 +0200

    modified basemanager file to make it a PDO singleton

--- models/BaseManager.php
+++ models/BaseManager.php
@@ -1,13 +1,27 @@
 <?php
 
 class BaseManager {
-	public static function dbConnect() {
-		try {
-		$db = new PDO('mysql:host=localhost;dbname=test_ocrp5;charset=utf8', 'root', '');
-		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		return $db;
-		} catch (PDOException $e) {
-			echo 'Connection failed: ' . $e->getMessage();
-		}	
+	private static $PDOInstance;
+
+	public function __construct($dsn=false, $username=false, $password=false) {
+		if(!self::$PDOInstance) {
+			$dsn = 'mysql:host=localhost;dbname=test_ocrp5;charset=utf8';
+			$username = 'root';
+			$password = '';
+			try {
+				self::$PDOInstance = new PDO($dsn, $username, $password);
+			} catch (PDOException $e) { 
+				die("PDO CONNECTION ERROR: " . $e->getMessage() . "<br/>");
+			}
+		}
+		return self::$PDOInstance;
+	}
+
+	public function prepare($statement) {
+		return self::$PDOInstance->prepare($statement);
+	}
+
+	public function query($statement) {
+		return self::$PDOInstance->query($statement);
 	}
 }
