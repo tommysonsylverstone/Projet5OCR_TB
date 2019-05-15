aab6ca1f5864af3e871d268e64dbb14f51c10316
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2019-05-15 14:38:33 +0200

    improved autoloader.php

--- autoloader/autoloader.php
+++ autoloader/autoloader.php
@@ -1,5 +1,11 @@
 <?php
 
-spl_autoload_register(function ($class) {
-	include 'models/'.$class.'.php';
-});
+class Autoloader {
+    static public function register() {
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+    }
+
+    static public function autoload($class) {
+            require 'models/' . $class . '.php';
+    }
+}
